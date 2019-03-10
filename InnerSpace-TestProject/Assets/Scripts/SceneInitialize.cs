a8251b6f0d624201145630fc1c94a823d0be11bd
victorocha11 <victorocha11@gmail.com>
2019-03-09 22:06:18 -0300

    Creating MainScene, bar progress and controller script.

--- InnerSpace-TestProject/Assets/Scripts/SceneInitialize.cs
+++ InnerSpace-TestProject/Assets/Scripts/SceneInitialize.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneInitialize : MonoBehaviour
 {
+    private string prefabPath = "Corridors/";
+    private string jsonPath = "Json/";
     // Start is called before the first frame update
     void Start()
     {
-        
+        //GameObject[] a = Resources.LoadAll<GameObject>(prefabPath) as GameObject[];
+        //foreach (var aux in a)
+        //{
+        //    Instantiate(aux);
+        //}
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        StartCoroutine(LoadSceneAsync(sceneIndex));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+
+            yield return null;
+        }
     }
 }
